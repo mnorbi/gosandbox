10f9807e594781f4d76b67c11604e7d495356996
mnorbi <norbert.madarasz@gmail.com>
2021-10-30 17:26:49 +0200

    composites/slice

--- composites/slice.go
+++ composites/slice.go
@@ -3,11 +3,26 @@ package composites
 // RemoveEmpty removes empty strings from sslice.
 // Modifies sslice and returns it.
 func RemoveEmpty(sslice []string) []string {
-	return []string{}
+	lo := 0
+	for hi := 0; hi < len(sslice); hi++ {
+		if len(sslice[hi]) > 0 && lo < hi {
+			sslice[lo] = sslice[hi]
+			sslice[hi] = ""
+		}
+		if len(sslice[lo]) > 0 {
+			lo++
+		}
+
+	}
+	return sslice[:lo]
 }
 
 // Remove the element with index k. Modifies v.
 func Remove(v []int, k int) []int {
-	// use built in function copy
-	return v
+	len := len(v)
+	if k < 0 || k >= len {
+		return v
+	}
+	copy(v[k:], v[k+1:])
+	return v[:len-1]
 }
